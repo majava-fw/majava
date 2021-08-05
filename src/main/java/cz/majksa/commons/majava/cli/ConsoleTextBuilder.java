8064aa238381e0b0ba0922184f6c253dba3a3f2a
majksa <info@majksa.cz>
2021-08-05 22:46:51 +0200

    Utils

---
+++ src/main/java/cz/majksa/commons/majava/cli/ConsoleTextBuilder.java
@@ -0,0 +1,138 @@
+/*
+ *  majava - cz.majksa.commons.majava.cli.ConsoleTextBuilder
+ *  Copyright (C) 2021  Majksa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package cz.majksa.commons.majava.cli;
+
+import javax.annotation.Nonnull;
+
+/**
+ * <p><b>Class {@link cz.majksa.commons.majava.cli.ConsoleTextBuilder}</b></p>
+ * A fancy way to print modified text to console
+ *
+ * @author majksa
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public final class ConsoleTextBuilder {
+
+    public static final int WIDTH = 100;
+
+    @Nonnull
+    private final StringBuilder builder = new StringBuilder();
+
+    private int width = 0;
+
+    /**
+     * Appends a text to the builder
+     *
+     * @param csq the text to be appended
+     * @return {@link cz.majksa.commons.majava.cli.ConsoleModifiers}
+     */
+    @Nonnull
+    public ConsoleTextBuilder append(CharSequence csq) {
+        if (width + csq.length() > WIDTH) {
+            final int diff = WIDTH - width;
+            append(csq.subSequence(0, diff));
+            builder.append("\n");
+            if (diff < csq.length() - 1) {
+                builder.append(csq, diff, csq.length());
+            }
+            width = csq.length() - diff;
+        } else {
+            builder.append(csq);
+            width += csq.length();
+        }
+        return this;
+    }
+
+    /**
+     * Apply a modification to the following text
+     *
+     * @param modification the modification
+     * @return {@link cz.majksa.commons.majava.cli.ConsoleModifiers}
+     */
+    @Nonnull
+    public ConsoleTextBuilder modify(@Nonnull ConsoleModifiers modification) {
+        builder.append(modification);
+        return this;
+    }
+
+    /**
+     * Reset all modifications of the following text
+     *
+     * @return {@link cz.majksa.commons.majava.cli.ConsoleModifiers}
+     */
+    @Nonnull
+    public ConsoleTextBuilder reset() {
+        return modify(ConsoleModifiers.RESET);
+    }
+
+    /**
+     * Adds a new line and resets
+     *
+     * @return {@link cz.majksa.commons.majava.cli.ConsoleModifiers}
+     */
+    @Nonnull
+    public ConsoleTextBuilder newLine() {
+        return newLine(true);
+    }
+
+    /**
+     * Adds a new line and resets if <code>reset</code> is true
+     *
+     * @param reset if the modifications should be reset
+     * @return {@link cz.majksa.commons.majava.cli.ConsoleModifiers}
+     */
+    @Nonnull
+    public ConsoleTextBuilder newLine(boolean reset) {
+        if (reset) {
+            reset();
+        }
+        builder.append("\n");
+        width = 0;
+        return this;
+    }
+
+    /**
+     * Builds a raw text
+     *
+     * @return the raw text
+     */
+    @Nonnull
+    public String build() {
+        newLine();
+        return builder.toString();
+    }
+
+    /**
+     * Prints the text to console
+     */
+    public void print() {
+        System.out.print(build());
+        clear();
+    }
+
+    /**
+     * Clears the builder
+     */
+    public void clear() {
+        builder.setLength(0);
+        width = 0;
+    }
+
+}
---
+++ src/main/java/cz/majksa/commons/majava/serializable/SerializableThrowable.java
@@ -0,0 +1,149 @@
+/*
+ *  majava - cz.majksa.commons.majava.serializable.SerializableThrowable
+ *  Copyright (C) 2021  Majksa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package cz.majksa.commons.majava.serializable;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <p><b>Class {@link SerializableThrowable}</b></p>
+ *
+ * @author majksa
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class SerializableThrowable implements Serializable {
+
+    public static final String NO_MESSAGE = "No message";
+
+    private static final long serialVersionUID = 8451803073914264980L;
+    private static final Map<Throwable, SerializableThrowable> throwables = new HashMap<>();
+
+    private String message;
+    private String className;
+    private StackTraceElement[] stackTrace;
+
+    /**
+     * Gets or creates a new serializable throwable
+     *
+     * @param throwable the throwable to find by or create from
+     * @return the serializable throwable
+     */
+    public static @NonNull SerializableThrowable from(@NonNull Throwable throwable) {
+        if (!exists(throwable)) {
+            throwables.computeIfAbsent(throwable, unused -> new SerializableThrowable(
+                    throwable.getMessage() == null ? NO_MESSAGE : throwable.getMessage(),
+                    throwable.getClass().getName(),
+                    throwable.getStackTrace()
+            ));
+        }
+        return get(throwable);
+    }
+
+    /**
+     * Gets a serializable throwable
+     *
+     * @param throwable the throwable to find by
+     * @return the serializable throwable
+     * @throws java.lang.NullPointerException if the throwable does not exist
+     */
+    public static @NonNull SerializableThrowable get(@NonNull Throwable throwable) throws NullPointerException {
+        for (SerializableThrowable value : throwables.values()) {
+            if (value.compare(throwable)) {
+                return value;
+            }
+        }
+        throw new NullPointerException();
+    }
+
+    /**
+     * Tests if a throwable exists
+     *
+     * @param throwable the throwable to find if exists
+     * @return true if it does
+     */
+    public static boolean exists(@NonNull Throwable throwable) {
+        for (SerializableThrowable value : throwables.values()) {
+            if (value.compare(throwable)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * The function for serialization saving
+     *
+     * @param stream the output stream
+     * @throws IOException if an error occurs
+     */
+    private void writeObject(ObjectOutputStream stream) throws IOException {
+        stream.writeObject(message);
+        stream.writeObject(className);
+        stream.writeObject(stackTrace);
+    }
+
+    /**
+     * The function for serialization reading
+     *
+     * @param stream the input stream
+     * @throws IOException            if an error occurs
+     * @throws ClassNotFoundException if the classes do not match
+     */
+    private void readObject(ObjectInputStream stream) throws IOException, ClassNotFoundException {
+        message = (String) stream.readObject();
+        className = (String) stream.readObject();
+        stackTrace = (StackTraceElement[]) stream.readObject();
+    }
+
+    /**
+     * Compares a throwable with the serialized throwable
+     *
+     * @param throwable the throwable to compare with
+     * @return if the throwables have same info
+     */
+    public boolean compare(Throwable throwable) {
+        if (throwable.getMessage() == null) {
+            if (!message.equals(NO_MESSAGE)) {
+                return false;
+            }
+        } else {
+            if (!message.equals(throwable.getMessage())) {
+                return false;
+            }
+        }
+        if (!className.equals(throwable.getClass().getName())) {
+            return false;
+        }
+        return Arrays.equals(throwable.getStackTrace(), stackTrace);
+    }
+
+}
